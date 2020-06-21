4ba61c93f82deeb9820d2788c30f6403676e2f41
Alexparshh <alexparshh@gmail.com>
2020-06-20 23:40:55 -0400

    moved state up/refactored PersonList

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -1,19 +1,52 @@
 import React, { Component, StrictMode } from "react";
 import PersonList from './PersonList';
+import Person from './Person';
 
 
 class Application extends Component {
     state = { 
-        time: null,
-        people: [],
+
+        people: [
+            <Person key="1" value="1"></Person>,
+            <Person key="2" value="2"></Person>,
+            <Person key="3" value="3"></Person>,
+          ],
 
      }
+
+
+    handleAddPerson = () => {
+
+        this.setState({
+          people: [
+            ...this.state.people,
+            <Person
+              key={this.state.people.length + 1}
+              value={this.state.people.length + 1}
+            ></Person>,
+          ],
+        });
+      };
+
+    handleDeletePerson = () => {
+        let people = this.state.people.slice(0, -1);
+        this.setState({
+          people: people,
+        });
+      };
+
+
+
     render() { 
         return ( 
             <div>
                 <h1>Linq</h1>
-                <PersonList></PersonList>
+                <PersonList
+                people = {this.state.people} 
+                onAddPerson = {()=>this.handleAddPerson()}
+                onDeletePerson = {()=>this.handleDeletePerson()}></PersonList>
 
+                <button>Calculate</button>
                 
             </div>
          );
--- src/components/PersonList.jsx
+++ src/components/PersonList.jsx
@@ -1,46 +1,19 @@
 import React, { Component } from "react";
-import Person from "./Person";
+
 
 class PersonList extends Component {
-  state = {
-    people: [
-      <Person key="1" value="1"></Person>,
-      <Person key="2" value="2"></Person>,
-      <Person key="3" value="3"></Person>,
-    ],
-  };
-
-  handleAddPerson = () => {
-    console.log(this.state.people.length);
-    this.setState({
-      people: [
-        ...this.state.people,
-        <Person
-          key={this.state.people.length + 1}
-          value={this.state.people.length + 1}
-        ></Person>,
-      ],
-    });
-  };
-
-  handleDeletePerson = () => {
-    let people = this.state.people.slice(0, -1);
-    this.setState({
-      people: people,
-    });
-  };
 
   render() {
     return (
       <div>
-        <button size="lg" onClick={this.handleAddPerson}>
+        <button onClick={this.props.onAddPerson}>
           Add Person
         </button>
-        <button size="lg" onClick={this.handleDeletePerson}>
+        <button onClick={this.props.onDeletePerson}>
           Delete Person
         </button>
 
-        {this.state.people}
+        {this.props.people}
       </div>
     );
   }

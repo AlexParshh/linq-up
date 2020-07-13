d194840cfcd221e9e291e2f2282ecaec1f581c7e
Alexparshh <alexparshh@gmail.com>
2020-07-13 15:49:23 -0400

    make it better

---
+++ build/precache-manifest.10810c0737f6b0ff4f12f317f998ecfb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "795ea8ea9085e8a94a067c46e88574ca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36afcce08fe551b0c0aa",
+    "url": "/static/css/2.1883a104.chunk.css"
+  },
+  {
+    "revision": "9ae00145f30d11d072ec",
+    "url": "/static/css/main.38d9e7d1.chunk.css"
+  },
+  {
+    "revision": "36afcce08fe551b0c0aa",
+    "url": "/static/js/2.7f074e2d.chunk.js"
+  },
+  {
+    "revision": "1adb110813a30163ca679a14e0de12c3",
+    "url": "/static/js/2.7f074e2d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ae00145f30d11d072ec",
+    "url": "/static/js/main.7deb9445.chunk.js"
+  },
+  {
+    "revision": "8437200caf1604ec3346",
+    "url": "/static/js/runtime-main.7f7fa5ce.js"
+  }
+]);
